a947188b73d2b214eea7ab6b46d8380d95e22f1c
IgooorGP <igor.feq@gmail.com>
2017-06-08 11:44:19 -0300

    Added javadoc to Queue.java and Stack.java. SinglyLinkedList.java has been moved to the same folder for the proper compilation of Queue.java and Stack.java

--- stacks-queues-deques/Queue.java
+++ stacks-queues-deques/Queue.java
@@ -1,63 +1,121 @@
-/**
- *
- * This class represents a Queue data structure.
- *
- * @author Igor G. Peternella
- * @date 05-30-2017
- *
- */
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * This class consists of an implementation of a queue data structure (follows FIFO policy) 
+ * which is built on the top of a singly linked list data structure.
+ * This class implements the Iterable interface which uses SinglyLinkedList class.
+ * iterator.
+ *
+ * @author Igor G. Peternella
+ * @date 06-02-2017
+ */
+
 public class Queue<T> implements Iterable<T> {
 
-    private SinglyLinkedList<T> singlyLinkedList;    
-    private int size;
+    private SinglyLinkedList<T> singlyLinkedList;  // reference to a singly linked list
+    private int size;                              // size of the queue
 
+    /**
+     * Default constructor. Intializes an empty queue and sets
+     * singlyLinkedList reference var to an empty singly linked list.
+     */
+    
     public Queue() {
 	singlyLinkedList = new SinglyLinkedList<T>();
 	size = 0;
     }
 
+    /**
+     * Returns true if the queue is empty (size is zero).
+     *
+     * @return true if the queue is empty and false otherwise.
+     */
+
     public boolean isEmpty() {
 	return size == 0;
     }
+    
+    /**
+     * Returns the number of elements of the queue.
+     *
+     * @return the size (number of elements) of the queue.
+     */
 
     public int size() {
 	return size;
     }
 
+    /**
+     * Inserts an item at the beginning of the queue.
+     * Complexity: O(1).
+     *
+     * @param item is the item to be inserted.
+     */
+    
     public void enqueue(T item) {
+	// inserts the item at the end of a singly linked list
+	// to follow FIFO policy
+	// which is O(1) operation
 	singlyLinkedList.rightInsert(item);
 	++size;
     }
 
+    /**
+     * Removes the first item of the queue.
+     * Complexity: O(1).
+     *
+     * @return the first item of queue which follows the FIFO policy.
+     * @throws java.util.NoSuchElementException if the queue is empty.
+     */
+
     public T dequeue() {
-	if (size == 0) {
+	if (isEmpty()) {
 	    throw new NoSuchElementException("Empty queue.");
 	}
 	
 	--size;
+
+	// returns the first item of the singly linked list
+	// which is O(1) operation
 	return singlyLinkedList.removeAt(0);
     }
 
-    public T getAt(int ix) {
-	if (!isValidIndex(ix)) {
-	    throw new NoSuchElementException("Invalid queue index.");
-	}
-	
-	return singlyLinkedList.getItemAt(ix);
-    }
+    /**
+     * Returns the first item of the queue but does not remove it.
+     * Complexity: O(1).
+     *
+     * @return the first item of the queue.
+     * @throw java.util.NoSuchElementException if the queue is empty.
+     */
 
     public T peek() {
-	if (size == 0) {
+	if (isEmpty()) {
 	    throw new NoSuchElementException("Empty queue.");
 	}
 	
 	return singlyLinkedList.getItemAt(0);
     }
-    
+
+    /**
+     * Returns the item at a given index position of the queue.
+     * Complexity: O(N).
+     *
+     * @param ix is the index of the desired element.
+     * @return the item of the queue specified by the given index.
+     * @throw java.util.NoSuchElementException if ix is invalid.
+     */
+
+    public T getAt(int ix) {
+	if (!isValidIndex(ix)) {
+	    throw new NoSuchElementException("Invalid queue index.");
+	}
+
+	// uses singly linked list method which is O(N)
+	return singlyLinkedList.getItemAt(ix);
+    }
+
+    // helper method that returns true if an index is invalid
     private boolean isValidIndex(int ix) {
 	if (ix < 0 || ix >= size) {
 	    return false;
@@ -65,12 +123,15 @@ public class Queue<T> implements Iterable<T> {
 	    return true;
 	}
     }
-    
+
+    // Iterable interface implementation for the queue data structure
     public Iterator<T> iterator() {
-	// returns the iterator from the SinglyLinkedList class!
+	// returns the iterator from the SinglyLinkedList class
+	// which follows the Iterator<T> interface
 	return singlyLinkedList.iterator();
     }    
 
+    // unit testing
     public static void main(String[] args) {
 	Queue<Integer> q = new Queue<Integer>();
 
--- stacks-queues-deques/Stack.java
+++ stacks-queues-deques/Stack.java
@@ -1,60 +1,123 @@
-/**
- *
- * This class represents a Stack data structure.
- *
- * @author Igor G. Peternella
- * @date 05-30-2017
- *
- */
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * This class consists of an implementation of a stack data structure (follows LIFO policy) 
+ * which is built on the top of a singly linked list data structure.
+ * This class implements the Iterable interface which uses SinglyLinkedList class.
+ * iterator.
+ *
+ * [!] Make sure that SinglyLinkedList.java is in the same directory as this file [!]
+ *
+ * @author Igor G. Peternella
+ * @date 06-03-2017
+ */
+
 public class Stack<T> implements Iterable<T>{
 
-    SinglyLinkedList<T> singlyLinkedList;
-    int size;
+    private SinglyLinkedList<T> singlyLinkedList;     // reference to a singly linked list
+    private int size;                                 // size of the stack
+
+    /**
+     * Default constructor. Intializes an empty stack and sets
+     * singlyLinkedList reference var to an empty singly linked list.
+     */
     
     public Stack() {
 	singlyLinkedList = new SinglyLinkedList<T>();
 	size = 0;
     }
 
+    /**
+     * Returns true if the stack is empty (size is zero).
+     *
+     * @return true if the stack is empty and false otherwise.
+     */
+
     public boolean isEmpty() {
 	return size == 0;
     }
 
+    /**
+     * Returns the number of elements of the stack.
+     *
+     * @return the size (number of elements) of the stack.
+     */
+    
     public int size() {
 	return size;
     }
 
+    /**
+     * Inserts an item on the top of the stack.
+     * Complexity: O(1).
+     *
+     * @param item is the item to be inserted.
+     */
+    
     public void push(T item) {
+	// inserts the item at the beginning of the singly linked list
+	// to follow LIFO policy
+	// O(1) operation
 	singlyLinkedList.leftInsert(item);
 	++size;
     }
 
+    /**
+     * Removes the last added element of the stack (top item).
+     * Complexity: O(1).
+     *
+     * @return the last added element to the stack which follows LIFO policy.
+     * @throws java.util.NoSuchElementException if the stack is empty.
+     */
+    
     public T pop() {
-	if (size == 0) {
-	    throw new NoSuchElementException("Empty Stack.");
+	if (isEmpty()) {
+	    throw new NoSuchElementException("Empty stack.");
 	}
 	
 	--size;
 
+	// removes the last LEFT inserted item (top of the stack)
+	// which is a O(1) operation
 	return singlyLinkedList.removeAt(0);
     }
 
+    /**
+     * Returns the last added item to the stack (top item) but does not remove it.
+     * Complexity: O(1).
+     *
+     * @return the top item of the stack.
+     * @throw java.util.NoSuchElementException if the stack is empty.
+     */
+    
     public T peek() {
+	if (isEmpty()) {
+	    throw new NoSuchElementException("Empty stack.");
+	}
+	
 	return singlyLinkedList.getItemAt(0);
     }
 
+    /**
+     * Returns the item at a given index position of the stack.
+     * Complexity: O(N).
+     *
+     * @param ix is the index of the desired element.
+     * @return the item of the stack specified by the given index.
+     * @throw java.util.NoSuchElementException if ix is invalid.
+     */
+    
     public T getAt(int ix) {
 	if (!isValidIndex(ix)) {
 	    throw new NoSuchElementException("Invalid stack index.");
 	}
-	
+
+	// uses singly linked list method which is O(N)
 	return singlyLinkedList.getItemAt(ix);
     }
 
+    // helper method that returns true if an index is invalid
     private boolean isValidIndex(int ix) {
 	if (ix < 0 || ix >= size) {
 	    return false;
@@ -62,11 +125,15 @@ public class Stack<T> implements Iterable<T>{
 	    return true;
 	}
     }
-    
+
+    // Iterable interface implementation for the stack data structure
     public Iterator<T> iterator() {
+	// returns the iterator from the SinglyLinkedList class
+	// which follows the Iterator<T> interface
 	return singlyLinkedList.iterator();
     }
-    
+
+    // unit testing
     public static void main(String[] args) {
 	Stack<Integer> s = new Stack<Integer>();
 
