f9a3c48bb04fd79ee10edacedcc2dce0943954b8
IgooorGP <igor.feq@gmail.com>
2017-06-08 12:51:55 -0300

    Added javadoc for Selection.java

--- sorting/Selection.java
+++ sorting/Selection.java
@@ -1,24 +1,31 @@
 /**
- * This is a class that implements the elementary Selection sort.
- * All methods are based on a Comparable type to ensure that objects
- * used by these methods have the compareTo() method.
+ * This class consists of an implementation of the elementary Selection Sort algorithm.
+ * All methods use the Comparable data type to ensure that objects used by the
+ * methods of this class have implemented the Comparable interface i.e. this class can
+ * be used to sort Integer, Double, String, etc. class types.
+ * 
+ * The sort() method of this class uses a Comparable[] array type because the O(1) 
+ * indexing of the arrays. The sort() method is implemented in ascending order.
+ *
  * The methods of this class are static so no instantiation is required
- * which makes the class more suited for practical use. 
- * Based on the book Algorithms by Robert Sedgewick and Kevin Wayne.
+ * which makes the class more suitable for practical use, e.g., to sort a Comparable
+ * array one could use the sort() method: 
+ *
+ *              Selection.sort(arrayReference)
+ *
+ * Based on Algorithms (4th ed.) by Robert Sedgewick and Kevin Wayne.
  * 
  * @author Igor G. Peternella
- * @date 06-01-2017
+ * @date 06-05-2017
  */
 
 public class Selection {
 
     /**
      * Method that sorts a Comparable array using Selection sort.
-     * This sorting has time complexity O(N^2) of comparisons.
-     * It is and inplace method and requires no extra space
-     * complexity. Insensitive to input (arr) algorithm.
+     * Complexity: O(N^2)
      *
-     * @params arr is a Comparable array.
+     * @param arr is a Comparable array.
      */
     
     public static void sort(Comparable[] arr) {
@@ -26,27 +33,29 @@ public class Selection {
 
 	for (int i = 0; i < size; ++i) {
 	    
-	    // minIndex represents a[i] in the beginning
+	    // minIndex represents arr[i]
 	    int minIndex = i; 
 	    for (int j = i + 1; j < size; ++j) {
 
-		// if a[j] is smaller than a[i] we save
+		// if arr[j] is smaller than arr[i] we save
 		// j as the minIndex for the iterations
 		if (isLess(arr[j], arr[minIndex])) {
+		    // a[j] holds the smallest value
 		    minIndex = j;
 		}		
 	    }
 
-	    // swap a[i], a[minIndex] of the array arr
+	    // swaps arr[i], arr[minIndex]
 	    swap(arr, i, minIndex);
 	}
     }
 
     /**
-     * Auxiliary method for swaping to elements of a Comparable array.
+     * Swaps two elements of a Comparable array.
      * 
-     * @params arr is a reference to a Comparable array; ix1 and ix2 are
-     * the indexes of the elements to be swapped on the array.
+     * @param arr is a reference to a Comparable array.
+     * @param ix1 is the index of the first element to be swapped on the array.
+     * @param ix2 is the index of the second element to be swapped on the array.
      */
     
     public static void swap(Comparable[] arr, int ix1, int ix2) {
@@ -56,23 +65,24 @@ public class Selection {
     }
     
     /**
-     * Auxiliary method to compare two Comparable objects. Returns
-     * true if object one is less than object another.
+     * Compares two Comparables objects.
      * 
-     * @params one and another are Comparable objects.
-     * @returns true if one object is less than another object. Returns
+     * @param one is one of the objects to be compared.
+     * @param another is the other object to be compared.
+     * @return true if the one object is less than the another object. Returns
      * false otherwise.
      */
     
     public static boolean isLess(Comparable one, Comparable another) {
-	return one.compareTo(another) < 0; // -1 when less
+	// .compareTo() returns -1 when one < another	
+	return one.compareTo(another) < 0; // 
     }
 
     /**
-     * Convenience method that returns true if a Comparable array is sorted.
+     * Convenience method that checks if a Comparable array is sorted.
      *
-     * @params arr is a reference to a Comparable array.
-     * @returns true if the array is sorted. Returns falseo otherwise.
+     * @param arr is a reference to a Comparable array.
+     * @return true if the array is sorted. Returns false otherwise.
      */
     
     public static boolean isSorted(Comparable[] arr) {
@@ -86,9 +96,10 @@ public class Selection {
     }
 
     /**
-     * Convenience method that shows all elements of a Comparable array.
+     * Convenience method that prints to standard output all elements
+     * of a Comparable array in an organized way, e.g., [1, 2, 3].
      * 
-     * @params arr is a reference to a Comparable array.
+     * @param arr is a reference to a Comparable array.
      */
     
     public static void show(Comparable[] arr) {
@@ -102,13 +113,19 @@ public class Selection {
 	}
     }
 
-    // unit tests
+    // unit testing
     public static void main(String[] args) {
 	Integer[] arr = {3, 10, -1, 0, 5, 4, 15, 7};
 
+	System.out.println("\nArray BEFORE sorting:");
 	show(arr);
-	System.out.println("Selection sorting...");
+	
+	System.out.println("\nSorting the array...\n");
 	Selection.sort(arr);
+	
+	System.out.println("Array AFTER sorting:");
 	show(arr);
+
+	System.out.println();
     }
 }
