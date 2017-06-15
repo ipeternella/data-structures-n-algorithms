3820a3020c94bd533f2e9090e846019015783c27
IgooorGP <igor.feq@gmail.com>
2017-06-15 00:43:31 -0300

    Implemented PriorityQueue.java which uses DynamicArray.java

---
+++ priority-queue/DynamicArray.java
@@ -0,0 +1,273 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * This class consists of an implementation of the dynamic array data structure which
+ * uses a common array as the main data structure for the algorithms.
+ * It contains a resize method that reallocates all elements of the current array to 
+ * a bigger or smaller one as necessary during run-time. 
+ * This class implements the Iterable interface.
+ *
+ * Based on Algorithms (4th ed.) by Robert Sedgewick and Kevin Wayne.
+ *
+ * @author Igor G. Peternella
+ * @date 05-30-2017
+ */
+
+public class DynamicArray<T> implements Iterable<T>{
+
+    private int INITIAL_SIZE = 1; // initial size of the main array
+    private T[] arr;              // the main array of this data structure
+    private int size;             // the size of the collection
+
+    /**
+     * Default constructor. Intializes an empty DynamicArray object with empty size and the
+     * main array of this data structure as a new Object[] type which is cast into T[] type. 
+     */
+    
+    public DynamicArray() {
+	size = 0;
+	arr = (T[]) new Object[INITIAL_SIZE];
+    }
+
+    /**
+     * Returns true if the DynamicArray is empty (size is zero).
+     *
+     * @return true if the structure is empty and false otherwise.
+     */
+    
+    public boolean isEmpty() {
+	return size == 0;
+    }
+
+    /**
+     * Returns the number of elements of the DynamicArray.
+     *
+     * @returns the size of the DynamicArray.
+     */
+
+    public int size() {
+	return size;
+    }
+
+    /**
+     * Appends an item to the end of the DynamicArray.
+     * Complexity: O(1) amortized.
+     *
+     * @param item is the item to be appended.
+     */
+
+    public void append(T item) {
+	arr[size] = item;
+	++size;
+
+	// resizes to a new array if necessary
+	if (size == arr.length) {
+	    resize(2 * size);
+	}
+    }
+
+    /**
+     * Overloaded method that appends an item to a valid index of the 
+     * DynamicArray and shifts elements to the right.
+     * Complexity: O(N).
+     *
+     * @param item is the item to be added.
+     * @param is a valid index to insert the item.
+     * @throws java.lang.ArrayIndexOutOfBoundsException if an invalid ix is passed.
+     */
+    
+    public void append(T item, int ix) {
+	// isValidIndex is a helper method
+	if (!isValidIndex(ix)) {
+	    throw new java.lang.ArrayIndexOutOfBoundsException("Invalid Index.");
+	} 	
+
+	// shifts elements to the right after the insertion
+	for (int i = size - 1; i >= ix; --i) {
+	    arr[i + 1] = arr[i];
+	}
+
+	// inserts item @ ix position
+	arr[ix] = item;
+	++size;
+
+	// resizes to a new array if necessary
+	if (size == arr.length) {
+	    resize(2 * size);
+	}
+    }
+    
+    /**
+     * Gets the item at a given index of the DynamicArray.
+     * Complexity: O(1).
+     *
+     * @param is a valid index to insert the item.
+     * @returns item at the given index.
+     * @throws java.lang.ArrayIndexOutOfBoundsException if an invalid ix is passed.
+     */
+
+    public T getAt(int ix) {
+	if (isValidIndex(ix)) {
+	    return arr[ix];
+	} else {
+	    throw new java.lang.ArrayIndexOutOfBoundsException("Invalid Index.");
+	}
+    }
+
+    /**
+     * Removes and returns the item at the end of the DynamicArray.
+     * Complexity: O(1) amortized.
+     *
+     * @returns item at the end of the DynamicArray.
+     * @throws java.util.NoSuchElementException if this method is invoked on a empty DynamicArray.
+     */
+   
+    public T pop() {
+	if (isEmpty()) {
+	    throw new java.util.NoSuchElementException("Empty Dynamic Array.");
+	}
+	
+	T item = arr[size - 1];
+	arr[size - 1] = null; // avoids loitering is removed
+	--size;
+
+	// reduces arr length by half if0 25% occupation is reached
+	if (size < (float) arr.length/4) {
+	    resize(arr.length/2);
+	}
+	
+	return item;
+    }
+    
+    /**
+     * Removes and returns the item at a given index of the DynamicArray.
+     * Complexity: O(N).
+     *
+     * @param ix is a valid index.
+     * @returns item at the index position of the DynamicArray.
+     * @throws java.util.NoSuchElementException if this method is invoked on a empty DynamicArray.
+     * @throws java.lang.ArrayIndexOutOfBoundsException if an invalid ix is passed. 
+     */
+    
+    public T pop(int ix) {
+	if (isEmpty()) {
+	    throw new java.util.NoSuchElementException("Empty Dynamic Array.");
+	}	
+	
+	if (!isValidIndex(ix)) {
+	    throw new java.lang.ArrayIndexOutOfBoundsException("Invalid Index.");
+	}
+	
+	T item = arr[ix];
+
+	// shifts elements to the left
+	for (int i = ix; i < size - 1; ++i) {
+	    arr[i] = arr[i + 1];	    
+	}
+	
+	arr[size - 1] = null; // after shifting to left last element is set to null (empty space)
+	--size;
+
+	// reduces arr length by half if 25% occupation is reached
+	if (size < (float) arr.length/4) {
+	    resize(arr.length/2);
+	}
+	
+	return item;
+    }
+
+    // resize helper method that copies every element of the current array into a new array
+    // with a bigger of small capacity given by the argument newCapacity.    
+    private void resize(int newCapacity) {
+	T[] newArr = (T[]) new Object[newCapacity];
+
+	// copy arr array into newArr array
+	for (int i = 0; i < size; ++i) {
+	    newArr[i] = arr[i];
+	}
+
+	// change reference
+	arr = newArr;
+    }
+
+    /**
+     * Replaces an item at the given index position.
+     * Complexity: O(1).
+     *
+     * @param ix is a valid index.
+     * @param item is the item to replace the previous value located at the given index.
+     * @throws java.util.NoSuchElementException if this method is invoked on a empty DynamicArray.
+     * @throws java.lang.ArrayIndexOutOfBoundsException if an invalid ix is passed.
+     */
+
+    public void replaceAt(int ix, T item) {
+	if (isEmpty()) {
+	    throw new java.util.NoSuchElementException("Empty Dynamic Array.");
+	}	
+	
+	if (!isValidIndex(ix)) {
+	    throw new java.lang.ArrayIndexOutOfBoundsException("Invalid Index.");
+	}
+
+	arr[ix] = item;
+    }
+
+    // helper method that returns true if an index is invalid.
+    private boolean isValidIndex(int ix) {
+	if (ix < 0 || ix >= size) {
+	    return false;
+	} else {
+	    return true;
+	}
+    }
+
+    // Iterable interface implementation for the DynamicArray data structure.
+    public Iterator<T> iterator() {
+	return new DynamicArrayIterator();
+    }
+
+    // Nested private class to create Iterator Objects for the DynamicArray data structure.
+    // Iterator interface implementation.
+    private class DynamicArrayIterator implements Iterator<T> {
+	int iteratorIx, collectionSize;
+
+	public DynamicArrayIterator() {
+	    iteratorIx = 0;
+	    collectionSize = size;
+	}
+
+	public boolean hasNext(){
+	    return iteratorIx < collectionSize;
+	}
+
+	public T next() {
+	    T item = arr[iteratorIx];
+	    ++iteratorIx;
+	    
+	    return item;
+	}
+
+	public void remove() {
+	    throw new UnsupportedOperationException("Unsafe operation not implemented.");
+	}
+    }
+    
+    // unit testing
+    public static void main(String[] args) {
+	DynamicArray<Integer> a = new DynamicArray<Integer>();
+
+	for (int i = 0; i < 10; ++i) {
+	    a.append(i);
+	}
+
+	a.append(-5, 5);
+	a.pop(5);
+	a.pop();
+	a.append(15);
+	
+	for (int i : a) {
+	    System.out.println(i);
+	}
+    }
+}    
---
+++ priority-queue/PriorityQueue.java
@@ -0,0 +1,295 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * This class consists of an implementation of a priority queue data structure
+ * which is built on the top of a dynamic array structure which is interpreted
+ * as a Binary Heap Tree.
+ *
+ * T is a bounded parametric type taht implements Comparable interface to allow
+ * the "priority" comparisons in the data structure. 
+ *
+ * [!] Make sure that DynamicArray.java is in the same directory as this file [!]
+ *
+ * @author Igor G. Peternella
+ * @date 06-10-2017
+ */
+
+public class PriorityQueue<T extends Comparable<T>> implements Iterable<T> {
+        
+    private DynamicArray<T> pq; // pq is a reference to a DynamicArray which is interpreted as a Heap
+    private int heapSize;       // heapSize is the DynamicArray (Heap) size
+
+    /** 
+     * The algorithms for this data structure requires a one-index-based DynamicArray and not zero-index-based one
+     * because of the interpretation of this array as a Binary Heap that starts at heapIndex = 1. Hence, the 
+     * indexes used by these algorithms are called virtual indexes (heapIndexes) in contrast to the real indexes 
+     * for arrays that start at index 0 such as Java's.
+     *
+     * Given the fact that:        virtual index == real index - 1  
+     * Methods isLess() and swap() have been implemented to subtract 1 from the virtual indexes passed
+     * to these methods to map to the real elements of the DynamicArray which is zero-index-based.
+     *
+     * Builds an empty priority queue based on a DynamicArray implementation.
+     */
+    
+    public PriorityQueue() {
+	pq = new DynamicArray<T>();
+	heapSize = 0;
+    }
+    
+    /**
+     * Returns true if the priority queue is empty (size is zero).
+     *
+     * @return true if the stack is empty and false otherwise.
+     */
+
+    public boolean isEmpty() {
+	return heapSize == 0;
+    }
+
+    /**
+     * Returns the number of elements of the priority queue.
+     *
+     * @return the size (number of elements) of the priority queue.
+     */
+    
+    public int size() {
+	return heapSize;
+    }
+
+    
+    /**
+     * Inserts an item at the proper position in the queue based on its priority.
+     * Complexity: O(log(N)). The usage of a Binary Heap allows this implementation
+     * to have this complexity. With ordinary linked lists or arrays we have O(N) which
+     * is awful for M inserts = O(M*N).
+     *
+     * @param item is the item to be inserted.
+     */
+
+    public void insert(T item) {
+	// appends an item at the end of the DynamicArray (as Last node of the heap)
+	pq.append(item);
+	
+	// increases heapSize
+	++heapSize;
+	
+	// swims up to reheapify from bottom up
+	swim(heapSize);	
+    }
+    
+    /**
+     * Removes and returns the item with highest priority on the priority queue.
+     * Complexity: O(log(N)). The usage of a Binary Heap allows this implementation
+     * to have this complexity. With ordinary linked lists or arrays we have O(N) which
+     * is awful for M delMax = O(M*N).
+     *
+     * @return item with the highest priority on the queue.
+     * @throws java.util.NoSuchElementException if this method is invoked on a empty priority queue.
+     */
+    
+    public T delMax() {
+	if (isEmpty()) {
+	    throw new java.util.NoSuchElementException("Empty priority queue.");
+	}
+	// picks the item at the top of the heap which is the biggest item (highest priority)
+	T item = pq.getAt(0);
+
+	// swaps the top node (biggest item) with the last node
+	swap(1, heapSize);
+
+	// biggest node is at the last position but violates heap order so we decrease heapSize
+	--heapSize;
+
+	// removes item from the DynamicArray (Heap)
+	pq.pop();
+
+	// sinks down to reheapify from top to bottom
+	sink(1);
+
+	return item;
+    }
+    
+    /**
+     * Returns the biggest item (highest priority) on the priority queue but does not remove it.
+     *
+     * @return item with the highest priority on the queue.
+     * @throws java.util.NoSuchElementException if this method is invoked on a empty priority queue.
+     */
+    
+    public T max() {
+	if (isEmpty()) {
+	    throw new java.util.NoSuchElementException("Empty priority queue.");
+	}
+	
+	return pq.getAt(0);
+    }
+    
+    /*
+     * Goes down in a heap structure represented by an array and swaps children elements
+     * that are bigger than their parent to ensure heap order i.e. this method heapifies
+     * an array from top to bottom.
+     * 
+     * arr is a reference to a Comparable array that represents a heap.
+     * heapIndex is a virtual index used for one-based arrays (required by heap algorithm).
+     * heapSize is the array size (size of the heap).
+     */    
+    
+    private void sink(int heapIndex) {
+	// begins going down the heap if the starting index (parent) has
+	// at least a leftChild node so 2 * heapLevel < heapSize evals to true!
+	
+	// loops until a parent node with no children is found
+	// so 2 * heapIndex > heapSize
+	while (2 * heapIndex <= heapSize) {
+	    // heapIndex is a virtual index and so are leftChild and others	    
+	    int parent = heapIndex;                    // parent node
+	    int leftChild = 2 * heapIndex;             // first child node
+	    
+	    // assumes leftChild is the biggest 
+	    int biggestChild = leftChild;
+
+	    // checks if the leftChild has a rightChild sibling
+	    if (leftChild < heapSize) {
+		int rightChild = 2 * heapIndex + 1;        // second child node
+
+		// picks the biggest Sibling (right or left) to compare with parent
+		if (isLess(leftChild, rightChild)) {
+		    biggestChild = rightChild;
+		}
+	    }
+	    
+	    // if biggestChild is smaller than parent
+	    // than it follows the heap order so break
+	    if (isLess(biggestChild, parent)) {
+		break;
+	    } else {
+		// biggestChild is bigger than parent
+		// which violates heap order so swap
+		swap(biggestChild, parent);
+	    }
+	    	    
+	    // advances to biggest child
+	    // which will become the next parent
+	    // to move down on the heap for then next iteration
+	    heapIndex = biggestChild;
+	}
+    }
+
+    /*
+     * Goes up in a heap structure represented by an array and swaps a child and its parent until
+     * a bigger parent is found i.e. this method heapifies an array from top to bottom.
+     * 
+     * heapIndex is a virtual index used for one-based arrays (required by heap algorithm).
+     * heapSize is the array size (size of the heap).
+     */    
+    
+    private void swim(int heapIndex) {
+	// heapifies from bottom-up until the first node
+	// which has no parent (heapIndex = 1)
+	while (heapIndex > 1) {
+	    int child = heapIndex;
+	    int parent = heapIndex/2;
+
+	    // if child is bigger than its parent node
+	    // swap
+	    if (isLess(parent, child)) {
+		swap(parent, child);
+	    } else {
+		// if a a bigger parent is found
+		// then the heap order is ensured
+		break;
+	    }
+
+	    // moves up on the heap for the next iteration
+	    heapIndex = parent;
+	}
+    }
+
+    /*
+     * Swaps two elements of the DynamicArray. It uses DynamicArray's method
+     * replaceAt(index) to make the swap.
+     * 
+     * ix1 is the index of the first element to be swapped on the DynamicArray.
+     * ix2 is the index of the second element to be swapped on the DynamicArray.
+     */    
+    
+    private void swap(int ix1, int ix2) {
+	// corrects virtual indexes of the heap to
+	// map to real indexes of 0 based java arrays (which DynamicArray is based)
+	--ix1;
+	--ix2;
+	
+        T temp = pq.getAt(ix1);
+
+	// swaps items at the real indexes of the DynamicArray
+	pq.replaceAt(ix1, pq.getAt(ix2));
+	pq.replaceAt(ix2, temp);
+    }
+    
+    /*
+     * Compares two Comparables objects of the DynamicArray. It uses DynamicArray's method
+     * getAt(index) to get items.
+     * 
+     * ix1 is the index of the first object to be compared.
+     * ix2 is the index of the second object to be compared.
+     *
+     * returns true if the item at index ix1 is less than the item at the index ix2. Returns
+     * false otherwise.
+     */
+    
+    private boolean isLess(int ix1, int ix2) {
+	// corrects virtual indexes of the heap to
+	// map to real indexes of 0 based java arrays (which DynamicArray is based)
+	--ix1;
+	--ix2;
+
+	// gets the items at the real index from the Dynamic Array
+	Comparable one = pq.getAt(ix1);
+	Comparable another = pq.getAt(ix2);
+	
+	return one.compareTo(another) < 0; // -1 when less
+    }
+
+    public Iterator<T> iterator() {
+	return pq.iterator();
+    }
+           
+    // unit testing
+    public static void main(String[] args) {
+	PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+	pq.insert(2);
+	pq.insert(1);
+	pq.insert(5);
+	pq.insert(3);
+	pq.insert(20);
+
+	// for (int i : pq) {
+	//     System.out.println(i);
+	// }
+
+	System.out.println(pq.delMax());
+
+	pq.insert(7);
+	
+	System.out.println(pq.delMax());
+	System.out.println(pq.delMax());
+	System.out.println(pq.delMax());
+	System.out.println(pq.delMax());
+	System.out.println(pq.delMax());
+
+	System.out.println("size: " + pq.size());
+
+	pq.insert(3);
+	pq.insert(3);
+	pq.insert(25);
+
+	System.out.println(pq.delMax());
+	System.out.println(pq.delMax());
+	System.out.println(pq.delMax());
+	
+	System.out.println("size: " + pq.size());
+    }
+}
